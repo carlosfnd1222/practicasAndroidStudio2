adc8c7c9aba866b535444950e2638779b3341436
Carlos Fernando Soto Zepeda <fernandosz.ti19@utsjr.edu.mx>
2022-06-01 11:37:18 -0500

    Pantalla de registro

---
+++ app/src/main/java/com/example/loginapp/RegisterActivity.java
@@ -0,0 +1,31 @@
+package com.example.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        textView = findViewById(R.id.bntRegister1);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, Login.class);
+                startActivity(intent);
+
+                Toast.makeText(RegisterActivity.this, "You Cliked on text", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
